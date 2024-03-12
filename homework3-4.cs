d62ce5de8699c0b481ca497d04b8555c6e2f5cec
Elizabeth308 <153708638+Elizabeth308@users.noreply.github.com>
2024-03-12 19:25:37 +0300

    Add files via upload

--- homework3-4.cs
+++ homework3-4.cs
@@ -10,15 +10,23 @@ namespace homework3_4
         {
             Console.WriteLine("If you want to fill your array with random numbers, type \"false\", if you want to fill it yourself, type \"true\"");
             bool userchoise = bool.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------String array---------------------------");
             ProvideValueString stringvalue = new ProvideValueString();
-            OneDGeneric<string> stringArray = new OneDGeneric<string>(stringvalue);
+            OneDGeneric<string> stringArray1 = new OneDGeneric<string>(stringvalue, userchoise);
+            TwoDGeneric<string> stringArray2 = new TwoDGeneric<string>(stringvalue, userchoise);
+            Console.WriteLine("---------------------------Int array---------------------------");
             ProvideValueInt intvalue = new ProvideValueInt();
-            OneDGeneric<int> intArray = new OneDGeneric<int>(intvalue);
+            OneDGeneric<int> intArray1 = new OneDGeneric<int>(intvalue, userchoise);
+            TwoDGeneric<int> intArray2 = new TwoDGeneric<int>(intvalue, userchoise);
+            Console.WriteLine("---------------------------Double array---------------------------(enter value with comma)");
             ProvideValueDouble doublevalue = new ProvideValueDouble();
-            OneDGeneric<double> doubleArray = new OneDGeneric<double>(doublevalue);
+            OneDGeneric<double> doubleArray1 = new OneDGeneric<double>(doublevalue, userchoise);
+            TwoDGeneric<double> doubleArray2 = new TwoDGeneric<double>(doublevalue, userchoise);
+            Console.WriteLine("---------------------------Bool array---------------------------");
             ProvideValueBool boolvalue = new ProvideValueBool();
-            OneDGeneric<bool> boolArray = new OneDGeneric<bool>(boolvalue);
-            IPrinter[] printers = {stringArray, intArray, boolArray, doubleArray};
+            OneDGeneric<bool> boolArray1 = new OneDGeneric<bool>(boolvalue, userchoise);
+            TwoDGeneric<bool> boolArray2 = new TwoDGeneric<bool>(boolvalue, userchoise);
+            IPrinter[] printers = {stringArray1, stringArray2, intArray1, intArray2, doubleArray1, doubleArray2, boolArray1, boolArray2};
             foreach(var t in printers)
             {
                 t.Print();
